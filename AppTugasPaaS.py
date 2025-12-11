dba32033eb7ba0469160742b9e4ebd24fac28cb3
Muhammad Zibran Santosa <140435823+Akyellaa@users.noreply.github.com>
2025-12-11 19:38:49 +0700

    Add files via upload

---
+++ AppTugasPaaS.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Halo! Ini adalah Aplikasi PaaS Sederhana di Azure."
+
+if __name__ == '__main__':
+    app.run()
